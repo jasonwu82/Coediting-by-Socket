d558fe5b126a889ee1c4e2ab6c9adfd6b219f8ee
jasonwu82 <hungwei@umich.edu>
2018-01-27 12:08:56 -0500

    proper logging and remove unrelevants, use debug/deploy mode

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask_socketio import SocketIO, emit, join_room, leave_room, rooms
 import redis
 import os
 
+debug = True
+if 'ON_HEROKU' in os.environ:
+    debug = False
 
-#r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
-r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
+if debug:
+    r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+else:
+    r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -31,15 +36,14 @@ def chat_msg(message):
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    app.logger.error("connecting!!")
+    app.logger.info("connecting!!")
     emit('my_response', {'data': 'Connected!'})
 
 
 @socketio.on('join_room', namespace='/test')
 def join_by_path(message):
     path = message['path']
-    print(path)
-    app.logger.error('join_room with path: %s' % path)
+    app.logger.info('join_room: with path: %s' % path)
     join_room(path)
     if r.exists(path):
         content = r.get(path)
@@ -50,14 +54,14 @@ def join_by_path(message):
 def main_edit(message):
     global msg_count
     msg_count += 1
-    app.logger.error("edit_main!!")
-    app.logger.error(message['path'])
-    app.logger.error(message['data'])
+    app.logger.info("enter edit_main function:")
+    app.logger.info(message['path'])
+    app.logger.info(message['data'])
+    app.logger.info("current rooms: {0}".format(rooms()))
+
     emit('my_response', {'data': message['data'], 'count': msg_count}, broadcast=True)
-    print(rooms())
     for room in rooms():
         emit('edit_main_response', {'data': message['data']}, room=room)
     r.set(message['path'], message['data'])
-
 
 
